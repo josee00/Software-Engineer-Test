d439ea9981772fe3d3f780011b9a6188be3a208d
Jose Antonio Soto Guzmán <42388617+josee00@users.noreply.github.com>
2024-10-15 12:19:33 +0200

    Update CartService.java

--- CartApp/src/main/java/service/CartService.java
+++ CartApp/src/main/java/service/CartService.java
@@ -1,35 +1,7 @@
-@Service
-public class CartService {
-    private Map<String, Cart> carts = new HashMap<>();
-
-    public Cart createCart() {
-        Cart cart = new Cart();
-        carts.put(cart.getId(), cart);
-        return cart;
-    }
-
-
-    public void addProductToCart(String cartId, Product product) {
-        Cart cart = carts.get(cartId);
-        if (cart != null) {
-            cart.getProducts().add(product);
-            cart.setLastUpdated(LocalDateTime.now());
-        }
-    }
-
-
-    public Cart getCart(String cartId) {
-        return carts.get(cartId);
-    }
-
-
-    public void deleteCart(String cartId) {
-        carts.remove(cartId);
-    }
-
-    @Scheduled(fixedRate = 60000) 
-    public void removeInactiveCarts() {
-        LocalDateTime now = LocalDateTime.now();
-        carts.values().removeIf(cart -> Duration.between(cart.getLastUpdated(), now).toMinutes() >= 10);
-    }
+public interface CartService {
+    Cart createCart();
+    void addProductToCart(String cartId, Product product);
+    Cart getCart(String cartId);
+    void deleteCart(String cartId);
+    void removeInactiveCarts();
 }
